76e1d7c06549e7aacec0c84ed44064cc7b86e052
Pievisco89 <pie-89@live.it>
2021-05-04 15:12:59 +0200

    finito - devo fare bonus

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+/* Descrizione:
+ - Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+ - Cambiare il colore del testo cliccando su dei bottoni
+Bonus:
+1. Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+2. Cambiare la classe del testo scrivendola dentro a un input ma il cambio deve avvenire solo quando viene premuto invio */
+
+
+const app = new Vue ({
+
+  el: '#app',
+  data: {
+    msg: 'Questo è un messaggio stampato utilizzando i data!',
+    mioColore: ''
+  },
+  methods: {
+    cambioColore(classe){
+    this.mioColore = classe;
+    }
+  }
+
+});
+
